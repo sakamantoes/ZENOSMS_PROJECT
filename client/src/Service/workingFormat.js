daa853a77c24a3c7f899a319caf96ffbbc2c9ac1
leoTechHub <chinwekeleuchenna@gmail.com>
2026-06-27 22:50:34 +0100

    jfjkk

--- client/src/Service/workingFormat.js
+++ client/src/Service/workingFormat.js
@@ -9,3 +9,8 @@ export const getWorkingTools = async () => {
   const res = await api.get('/api/user/working/tools');
   return res.data;
 };
+
+export const workImageAndFormatePurchase = async (id) => {
+  const res = await api.post('/api/user/working/buy', { id });
+  return res.data;
+};
